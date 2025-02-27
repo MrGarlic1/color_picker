c5539f871b8963c61061a226d73272776c069679
MrGarlic1 <ben@samans.com>
2025-02-27 16:16:17 -0500

    fix accent algorithm

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def read_image(filename: str) -> np.array:
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
         img = img.reshape(-1, 4)
         img = img[img[:, 3] > 250][:, :3]
+        # img = img[:, 0:3]
     else:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img = img.reshape(-1, 3)
@@ -99,7 +100,7 @@ def get_primary_color(img: np.ndarray, frequencies, avg_color: np.ndarray, total
     img = img[0:len(frequencies), :]
 
     # ADJUSTMENT PARAMETERS
-    frequency_threshold = 0.02
+    frequency_threshold = 0.03
     frequency_weight = 1.2
     saturation_min_threshold = 0.05*255
     saturation_max_threshold = 0.65*255
@@ -150,7 +151,6 @@ def get_primary_color(img: np.ndarray, frequencies, avg_color: np.ndarray, total
     score = final_score[idx]
     primary_color_hls = img[idx, :].astype(np.uint8)
 
-    primary_color_rgb = cv2.cvtColor(np.array([[img[idx, :]]], dtype=np.uint8), cv2.COLOR_HLS2RGB)
     # print(color)
     # print(primary_color)
     print(f"Frequency Score: {frequency_score[idx]}")
@@ -159,6 +159,14 @@ def get_primary_color(img: np.ndarray, frequencies, avg_color: np.ndarray, total
     print(f"Hue Difference Score: {hue_difference_score[idx]}")
     # print(score)
     # print(frequencies[idx])
+
+    # Correction if best color is outside acceptable luminosity range
+    if primary_color_hls[1] < luminosity_min_threshold:
+        primary_color_hls[1] *= luminosity_min_threshold / primary_color_hls[1]
+    elif primary_color_hls[1] > luminosity_max_threshold:
+        primary_color_hls[1] *= luminosity_max_threshold / primary_color_hls[1]
+
+    primary_color_rgb = cv2.cvtColor(np.array([[primary_color_hls]], dtype=np.uint8), cv2.COLOR_HLS2RGB)
 
     return primary_color_rgb.reshape(3), primary_color_hls
 
@@ -182,16 +190,16 @@ def get_accent_color(img: np.ndarray, frequencies, avg_color: np.ndarray, total_
     img = img[0:len(frequencies), :]
 
     # ADJUSTMENT PARAMETERS
-    frequency_threshold = 0.01
+    frequency_threshold = 0.02
     frequency_weight = 1.2
     saturation_min_threshold = 0.55*255
     saturation_max_threshold = 0.95*255
-    saturation_weight = 1.75
+    saturation_weight = 2
     luminosity_min_threshold = 0.5*255
     luminosity_max_threshold = 0.75*255
-    luminosity_weight = 1
+    luminosity_weight = 0.5
     hue_difference_threshold = 50
-    hue_difference_weight = 0*0.6
+    hue_difference_weight = 1
     # hue_priority = "[equation to boost magenta color family and reduce tan]"
     # hue_weight = 0
 
@@ -219,13 +227,20 @@ def get_accent_color(img: np.ndarray, frequencies, avg_color: np.ndarray, total_
     adjustment = 2 if max(luminosity_score) < 0 else 0
     luminosity_score = luminosity_weight * (adjustment + luminosity_score/abs(max(luminosity_score)))
 
-    # Normalized hue score. A lower hue difference than the average image hue results in a higher score
-    hue_difference_score = (
-            1 - 1 / hue_difference_threshold**2 *
-            (img[:, 0] - avg_color[0])**2
+    # Normalized hue score. A higher hue difference than the primary image hue results in a higher score, capped at 1
+
+    hue_diff = np.abs(img[:, 0] - primary_color[0])
+    hue_diff = np.where(
+        hue_diff > 90,
+        180 - hue_diff,
+        hue_diff
     )
-    adjustment = 2 if max(hue_difference_score) < 0 else 0
-    hue_difference_score = hue_difference_weight * (adjustment + hue_difference_score)/abs(max(hue_difference_score))
+    print(hue_diff)
+
+    hue_difference_score = hue_difference_weight * np.minimum(
+            1 / hue_difference_threshold**2 * hue_diff**2 - 1,
+            1
+    )
 
     final_score = frequency_score + saturation_score + luminosity_score + hue_difference_score
 
@@ -233,7 +248,6 @@ def get_accent_color(img: np.ndarray, frequencies, avg_color: np.ndarray, total_
     score = final_score[idx]
     accent_color_hls = img[idx, :].astype(np.uint8)
 
-    accent_color_rgb = cv2.cvtColor(np.array([[img[idx, :]]], dtype=np.uint8), cv2.COLOR_HLS2RGB)
     # print(color)
     # print(accent_color)
     print(f"Frequency Score: {frequency_score[idx]}")
@@ -243,11 +257,19 @@ def get_accent_color(img: np.ndarray, frequencies, avg_color: np.ndarray, total_
     # print(score)
     # print(frequencies[idx])
 
+    # Correction if best color is outside acceptable luminosity range
+    if accent_color_hls[1] < luminosity_min_threshold:
+        accent_color_hls[1] *= luminosity_min_threshold / accent_color_hls[1]
+    elif accent_color_hls[1] > luminosity_max_threshold:
+        accent_color_hls[1] *= luminosity_max_threshold / accent_color_hls[1]
+
+    accent_color_rgb = cv2.cvtColor(np.array([[accent_color_hls]], dtype=np.uint8), cv2.COLOR_HLS2RGB)
+
     return accent_color_rgb.reshape(3), accent_color_hls
 
 
 def main():
-    filename = "Static/fern.png"
+    filename = "Static/firefox_bkgd.jpg"
     img_rgb, img_hls = read_image(filename=filename)
 
     total_pixels = len(img_rgb)
@@ -283,73 +305,6 @@ def main():
     cv2.imshow("Accent", np.full(shape=(100, 100, 3), fill_value=accent_color_bgr, dtype=np.uint8))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-
-    """
-    accent_candidates = [
-        color for color in groupings if
-        np.abs(color[0] - primary_color).sum() > accent_min_distance_from_primary and
-        np.abs(color[0] - avg_color).sum() > accent_min_distance_from_average and
-        color[1] > total_pixels * accent_freq_threshold and
-        # color[2] >= accent_stdev_threshold and
-        np.abs(color[0]).sum() > accent_min_brightness
-    ]
-    if not accent_candidates:
-        print("check1 fail accent")
-        accent_candidates = [
-            color for color in groupings if
-            np.abs(color[0] - primary_color).sum() > accent_min_distance_from_primary and
-            color[1] > total_pixels * accent_freq_threshold/100 and
-            np.abs(color[0]).sum() > accent_min_brightness
-            # color[2] >= accent_stdev_threshold
-        ]
-
-    if not accent_candidates:
-        print("check2 fail accent")
-        accent_candidates = [
-            color for color in groupings if
-            np.abs(color[0] - primary_color).sum() > accent_min_distance_from_primary and
-            color[1] > total_pixels * accent_freq_threshold/100
-        ]
-
-    accent_candidates = sorted(accent_candidates, key=lambda x: x[2], reverse=True)
-
-    accent_color = np.round(accent_candidates[0][0])
-
-    while np.abs(accent_color).sum() <= accent_min_brightness:
-        print(accent_color)
-        accent_color_hsv = colorsys.rgb_to_hsv(accent_color[2], accent_color[1], accent_color[0])
-        accent_color = colorsys.hsv_to_rgb(accent_color_hsv[0], accent_color_hsv[1], accent_color_hsv[2]*1.1)[::-1]
-
-    print(avg_color)
-    print(primary_color)
-    print(accent_color)
-
-    ## TESTING/REVIEWING CODE
-
-    avg_image = np.full(
-        shape=(100, 100, 3),
-        fill_value=avg_color,
-        dtype=np.uint8
-    )
-    primary_image = np.full(
-        shape=(100, 100, 3),
-        fill_value=primary_color,
-        dtype=np.uint8
-    )
-    accent_image = np.full(
-        shape=(100, 100, 3),
-        fill_value=accent_color,
-        dtype=np.uint8
-    )
-    cv2.imshow("average", avg_image)
-    cv2.imshow("original", img)
-    cv2.imshow("primary", primary_image)
-    cv2.imshow("accent", accent_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-    """
 
 
 if __name__ == "__main__":
